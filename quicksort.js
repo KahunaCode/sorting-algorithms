4ec6dec2ea337267a1c6a7bccbe8d9654e3355ef
James Moore <jameslmoore@gmail.com>
2017-06-24 18:16:37 -1000

    awesome

---
+++ quicksort.js
@@ -0,0 +1,21 @@
+function qs(arr){
+    if (arr.length > 1){
+        var pivot = arr[0];
+        var left = [];
+        var right = [];
+        for (var i = 1; i < arr.length; i++){
+
+            if (i < pivot){
+                left.push(arr[i])
+            }
+            else{
+                right.push(arr[i])
+            }
+        }
+        return qs(left).concat(pivot, qs(right));
+
+    }
+    else {
+        return arr;
+    }
+}
